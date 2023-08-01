f1b9337db3863992e71745fdb0f48089b3d59534
Canberk <highpriv@gmail.com>
2023-08-01 03:32:42 +0300

    update

--- views/dashboard/index.tsx
+++ views/dashboard/index.tsx
@@ -1,7 +1,44 @@
-export default function DashboardView() {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const DashboardView: React.FC = () => {
+  const [currencyData, setCurrencyData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = "https://www.tcmb.gov.tr/kurlar/today.xml";
+
+    const fetchCurrencyData = async () => {
+      try {
+        await axios
+          .get(apiUrl, {
+            headers: {
+              "Content-Type": "application/xml; charset=utf-8",
+            },
+          })
+          .then((response) => {
+            console.log(response.data);
+            setCurrencyData(response.data);
+          });
+      } catch (error) {
+        console.error("Error fetching currency data:", error);
+      }
+    };
+
+    fetchCurrencyData();
+  }, []);
+
   return (
-    <div className="w-full">
-      <div className="uppercase text-xl">Dashboard page</div>
+    <div>
+      {currencyData ? (
+        <div>
+          <h2>Currency Rates</h2>
+          <pre>{JSON.stringify(currencyData, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Loading currency data...</p>
+      )}
     </div>
   );
-}
+};
+
+export default DashboardView;
